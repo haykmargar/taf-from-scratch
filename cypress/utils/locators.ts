d655c38426d9419a3cc3ee61b2cd3bb52ebf462b
Hayk_Margaryan1 <Hayk_Margaryan1@epam.com>
2024-01-08 12:32:12 +0400

    minor fixes due to quality gates

--- cypress/utils/locators.ts
+++ cypress/utils/locators.ts
@@ -2,7 +2,7 @@ import { validProject } from '@Resources/constants';
 
 const locators = {
     login: 'input[placeholder="Login"]',
-    password: 'input[placeholder="Password"]',
+    passwordInput: 'input[placeholder="Password"]',
     submitButton: 'button[type="submit"]',
     dashboardTitle: 'span[title="All Dashboards"]',
     invalidUserNamePasswordError: '.notification-transition-enter-done p',
